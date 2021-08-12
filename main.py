3f8df6019db4bbf53554da4149474832e382c262
Martin-N-Menendez <menendez91@live.com.ar>
2021-08-12 19:59:51 -0300

    Reading railML

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import xml.etree.cElementTree as ET
 from xml.dom import minidom
 import re
 from RailML.railML import railML
-from RailML.Common.Metadata import Metadata
-
-#from RailML.Infrastructure import *
-#from RailML.Interlocking import *
+from RailML.Common import Metadata
+from RailML.Common import Common
+#from RailML.Infrastructure import Infrastructure
+#from RailML.Interlocking import Interlocking
+from RailML import aRailML
 
 XMLNS = "https://www.railml.org/schemas/3.1"
 XMLNS_DC ="http://purl.org/dc/elements/1.1/"
@@ -18,6 +19,8 @@ XSI_SCHEMALOCATION="https://www.railml.org/schemas/3.1 https://www.railml.org/sc
 VERSION = "3.1"
 
 ATTRIBUTES = {"xmlns":XMLNS , "xmlns:dc":XMLNS_DC , "xmlns:gml":XMLNS_GML , "xmlns:xsi":XMLNS_XSI , "xsi:schemaLocation":XSI_SCHEMALOCATION , "version":VERSION}
+
+RML = railML()
 
 #%%
 def print_leaves(root,leaf,tag):
@@ -32,7 +35,7 @@ def print_leaves(root,leaf,tag):
     for child in leaves:
         print(f'{child.tag[len(ns_leaf):]} | {child.attrib} | {child.text}')
 
-def metadata_set(root,leaf,tag,object):
+def get_leaves(root,leaf,tag):
     ns = re.match(r'{.*}', root.tag).group(0)
     if leaf != '':
         leaves = root.find(f"{ns}"+leaf)
@@ -40,6 +43,25 @@ def metadata_set(root,leaf,tag,object):
         leaves = root
         
     ns_leaf = '{'+tag+'}'
+    
+    return [leaves,ns_leaf]
+
+def get_leaves2(root,tag):
+    ns = re.match(r'{.*}', root.tag).group(0)
+        
+    leaves = []
+    leaf = []
+    ns_leaf = '{'+tag+'}'
+    
+    for i in root:  # HERE I CAN DO IT GENERIC
+        #print(x.tag[len(ns_leaf):])
+        leaves.append(i.tag[len(ns_leaf):])
+        leaf.append(i)
+    
+    return [leaf,leaves]
+
+def metadata_set(root,leaf,tag,object):
+    [leaves,ns_leaf] = get_leaves(root,leaf,tag)  
     
     object.Title = leaves[0].text
     object.Date = leaves[1].text
@@ -49,7 +71,45 @@ def metadata_set(root,leaf,tag,object):
     object.Subject = leaves[5].text
     object.Format = leaves[6].text
     object.Description = leaves[7].text
-    object.Publisher = leaves[8].text
+    object.Publisher = "GICSAFe"    #leaves[8].text
+
+def common_set(root,leaf,tag,object):        
+    [leaves,ns_leaf] = get_leaves(root,leaf,tag)    
+        
+    for child in leaves:
+        #print(f'{child.tag[len(ns_leaf):]} | {child.attrib} | {child.text}')
+        if child.tag[len(ns_leaf):] == "electrificationSystems":
+            print(f'>>{child.tag[len(ns_leaf):]}')
+            object.createElectrificationSystems
+        if child.tag[len(ns_leaf):] == "organizationalUnits":
+            print(f'>>{child.tag[len(ns_leaf):]}')
+            object.createOrganizationalUnits()
+            [organization,organization_leaf] = get_leaves(child,"infrastructureManager",XMLNS) 
+            if organization != None: 
+                print(f'>>>{organization.tag[len(organization_leaf):]}')
+                object.OrganizationalUnits.createInfrastructureManager()
+        if child.tag[len(ns_leaf):] == "speedProfiles":
+            print(f'>>{child.tag[len(ns_leaf):]}')
+            object.createSpeedProfiles()
+        if child.tag[len(ns_leaf):] == "positioning":
+            print(f'>>{child.tag[len(ns_leaf):]}')
+            object.createPositioningSystems()
+            
+            [sub_child,tag] = get_leaves2(child,XMLNS)
+
+            for i in range(len(tag)):
+                print(f'>>>{tag[i]}')
+                #print(sub_child[i])
+                [a,sub_tag] = get_leaves2(sub_child[i],XMLNS) 
+        
+                for j in range(len(sub_tag)):
+                    print(f'>>>>{sub_tag[j]}')
+                    
+            #if positioning != None: 
+            #    print(f'>>>{positioning.tag[len(positioning_leaf):]}')
+                #object.PosytioningSystems.createInfrastructureManager()
+            
+    #object.Title = leaves[0].text
 
 def metadata_to_XML(root_ML,metadata):
     #ET.SubElement(root_ML, "field3", name="blah").text = "some value1"
@@ -76,18 +136,25 @@ def load_xml(file):
     return root
     
 #%%
-def create_objects(root):
-    print('Creating RailML object')
-    RML = railML()
-    print('Creating Metadata object')
-    metadata = Metadata()
-    metadata_set(root,'metadata',XMLNS_DC,metadata)
-    RML.Metadata = metadata
+def set_objects(root):   
+    print('Setting aRailML')
+    RML.aRailML = aRailML.aRailML()
+    RML.aRailML.Version = VERSION
     
+    print('>Metadata')
+    RML.Metadata = Metadata.Metadata()
+    metadata_set(root,'metadata',XMLNS_DC,RML.Metadata)
     #print(RML.Metadata)
-    return RML
     
-def save_xml(RML,file):    
+    print('>Common')
+    #organizationalUnits
+    #positioning
+    RML.Common = Common.Common()
+    common_set(root,'common',XMLNS,RML.Common)  
+    #print(RML.Common)
+    
+    
+def save_xml(file):    
     root_ML = ET.Element("railML",attrib = ATTRIBUTES)
     metadata_ML = ET.SubElement(root_ML, "metadata")
 
@@ -103,8 +170,6 @@ def save_xml(RML,file):
         f.close()
     
 if __name__ == "__main__":
-    root = load_xml("RailML/Example_1.railml")
-    RML = create_objects(root)
-    save_xml(RML,"RailML/Example_2.railml")
-    
-# %%
+    root = load_xml("F:\PhD\RailML\Example_1.railml")   #A RELATIVE PATH DOESN'T WORK FOR PREVIEW!
+    set_objects(root)
+    save_xml("F:\PhD\RailML\Example_2.railml")  #A RELATIVE PATH DOESN'T WORK FOR PREVIEW!
---
+++ test.py
@@ -0,0 +1,15 @@
+
+#import sys
+#sys.path.append('.')
+import xml.etree.cElementTree as ET
+from xml.dom import minidom
+
+
+#TODO READ XML WITH PREVIEW
+
+#F:\PhD\App\Example_1.railml
+datasource = open("F:\PhD\App\Example_1.railml")
+
+
+
+
