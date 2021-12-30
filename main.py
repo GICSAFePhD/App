0fbc8e829804d5f33c11aaadfe7be53f2f94e5f6
Martin-N-Menendez <menendez91@live.com.ar>
2021-12-30 16:13:23 -0300

    Simplify is not working, some start hasn't got good direction

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ from ATG import ATG
 from ACG import ACG
 from AGG import AGG
 
-i = 8
+i = 6
+
+#TODO SIMPLIFY S07 y S17, TURN S24 AND S25
 
 INPUT_FILE  = "F:\PhD\RailML\Example_"+str(i)+".railml"
 OUTPUT_FILE = "F:\PhD\RailML\Example_"+str(i)+"_B.railml"
