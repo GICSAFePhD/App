7aad5b7bf231149a2c0fe3e9798347828e3386a0
Martin-N-Menendez <menendez91@live.com.ar>
2022-01-19 16:16:47 -0300

    Crossing+Platform solved and multi light signals updated

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from ATG import ATG
 from ACG import ACG
 from AGG import AGG
 
-i = 7
+i = 6
 
 INPUT_FILE  = "F:\PhD\RailML\Example_"+str(i)+".railml"
 OUTPUT_FILE = "F:\PhD\RailML\Example_"+str(i)+"_B.railml"
