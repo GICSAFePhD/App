74c17a908a5fa9f06a0f15456b3a946ecef3952b
Martin-N-Menendez <menendez91@live.com.ar>
2022-01-24 21:42:11 -0300

    Signals way introduced

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from ATG import ATG
 from ACG import ACG
 from AGG import AGG
 
-i = 6
+i = 8
 
 INPUT_FILE  = "F:\PhD\RailML\Example_"+str(i)+".railml"
 OUTPUT_FILE = "F:\PhD\RailML\Example_"+str(i)+"_B.railml"
